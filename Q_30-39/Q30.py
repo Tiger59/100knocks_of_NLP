048cba061e881dd691e7a4b44cf36085fb82ca26
t-kisimoto <kuroneko59@icloud.com>
2019-02-14 10:30:04 +0900

    add question 30

---
+++ Q_30-39/Q30.py
@@ -0,0 +1,24 @@
+def load_mecab(path):
+    f_in = open(path, 'r')
+    ans = []
+    sentence = []
+    for line in f_in:
+        line = line.split('\t')
+        if len(line) < 2:
+            if len(sentence) > 0:
+                ans.append(sentence)
+            sentence = []
+        else:
+            morpheme = line[1].split(',')
+            dic = {
+                'surface': line[0],
+                'base': morpheme[6],
+                'pos': morpheme[0],
+                'pos1': morpheme[1]
+            }
+            sentence.append(dic)
+
+    return ans
+
+path = '../data/neko.txt.mecab'
+print(load_mecab(path))
